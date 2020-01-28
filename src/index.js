76e165d80b56cfb03cfea4569d288a3de00e1849
Mariola Pasko <mariolapasko@Mariolas-MacBook-Air.local>
2020-01-28 20:08:02 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let input = document.getElementById("new-task-description")
+  let taskArray = []
+  let myToDos = document.getElementById("tasks")
+  let form = document.getElementById("create-task-form")
+
+  form.addEventListener('submit', e =>  {
+    e.preventDefault()
+    taskArray.push(input.value)
+    e.target.reset()
+    toDoList()
+  } )
+
+  function toDoList() {
+    myToDos.innerHTML = ""
+    for (let index = 0; index < taskArray.length; index++) {
+      let listItem = document.createElement('li')
+      listItem.innerText = `${taskArray[index]}`
+      let deleteItem = document.createElement('button')
+      deleteItem.innerText = `x`
+      deleteItem.class = `delete`
+      deleteItem.id = index
+      listItem.appendChild(deleteItem)
+      myToDos.appendChild(listItem)
+    }
+  }
+
+  myToDos.addEventListener('click', e => {
+    if (e.target.class === 'delete') {
+      taskArray.splice(e.target.id, 1)
+      toDoList()
+
+    }
+
+  })
+  
 });
