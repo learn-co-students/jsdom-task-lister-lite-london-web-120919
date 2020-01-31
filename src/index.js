32a58724e33dbfd4d6d9908b805798d4125d15c5
rmiri <re_miriuk@hotmail.com>
2020-01-31 13:45:38 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const newTaskForm = document.getElementById("create-task-form");
+  // const newTaskDesciption = document.getElementById("new-task-description");
+  // const newTaskPriority 
+
+  // List where the Todo will be strored
+  const newTaskUl = document.getElementById("tasks");
+
+  //attach event listeners
+ 
+
+  const createNewTask = event => {
+    event.preventDefault();
+    //so it doesn't try to submit to another page
+
+    //create the element
+    const newTaskDescription = document.getElementById("new-task-description"); 
+    const newTask = document.createElement('li');
+    newTask.innerText = newTaskDescription.value;
+    //add the style 
+
+    
+    //apend it to ul
+    newTaskUl.appendChild(newTask);
+
+
+  };
+
+  // it has to be after so it can't find 
+  newTaskForm.addEventListener("submit", createNewTask);
+
+
+
+})
